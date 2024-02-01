d93ae835f181ed20929db95ef9be94f6cb2431ab
Brice <brice.jarmoszko@groupecyberjustice.com>
2024-02-01 15:33:35 -0500

    add gitignore

---
+++ example.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Reproduction() {
+  return (
+    <div className="border-b-[length:var(--input\_border\_bottom\_width)]">
+      Some text
+    </div>
+  );
+}
