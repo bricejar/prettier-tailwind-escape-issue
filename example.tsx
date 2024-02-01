c0c8e0e358d1a7bc7e74c9448aecea4ef2ab4a08
Brice <brice.jarmoszko@groupecyberjustice.com>
2024-02-01 15:44:40 -0500

    save

--- example.tsx
+++ example.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export function Reproduction() {
   return (
-    <div className="border-b-[length:var(--input\_border\_bottom\_width)]">
+    <div
+      className={"border-b-[length:theme(spacing[input\_border-bottom-width])]"}
+    >
       Some text
     </div>
   );
